485f4c84c718a2731cf5a247afcb7b3b0bc2208a
Giuseppe Ursino <giursino@gmail.com>
2020-11-07 19:58:47 +0100

    fix settings filename and handle bad http status code

--- src/temperature-to-cloud.py
+++ src/temperature-to-cloud.py
@@ -12,11 +12,11 @@ from optparse import OptionParser
 
 _VERSION = "0.1.0"
 
-
-SETTINGS_FILE = os.path.dirname(os.path.realpath(__file__)) + '/' + __file__ + '.ini'
+# settings file is this filename with ini extension
+SETTINGS_FILE = os.path.realpath(__file__).replace('.py', '.ini')
 
 # settings for Tago.io cloud
-DEFAULT_TAGO_DEVICE_TOKEN_STRING = ""
+DEFAULT_TAGO_DEVICE_TOKEN = ""
 
 # settings for Tago.io cloud
 DEFAULT_TAGO_URL = "http://api.tago.io/data"
@@ -25,18 +25,23 @@ if __name__ == "__main__":
 
     # Read configuration file
     config = ConfigParser.ConfigParser()
+    print 'Reading %s settings file' % SETTINGS_FILE
     config.read(SETTINGS_FILE)
     TAGO_URL =  DEFAULT_TAGO_URL if not config.has_option('Tago', 'URL') else config.get('Tago', 'URL')
-    TAGO_DEVICE_TOKEN_STRING =  DEFAULT_TAGO_DEVICE_TOKEN_STRING if not config.has_option('Tago', 'DEVICE_TOKEN') else config.get('Tago', 'DEVICE_TOKEN')
+    TAGO_DEVICE_TOKEN =  DEFAULT_TAGO_DEVICE_TOKEN if not config.has_option('Tago', 'DEVICE_TOKEN') else config.get('Tago', 'DEVICE_TOKEN')
 
     # Read command line
     parser = OptionParser(version=_VERSION)
     parser.add_option("-u", "--tago-url", dest="tago_url", default=TAGO_URL,
-                        help="Tago.io server URL.")
-    parser.add_option("-d", "--device-token", dest="tago_device_token", default=TAGO_DEVICE_TOKEN_STRING,
-                        help="Tago.io device token.")
+                        help="Tago server URL.")
+    parser.add_option("-d", "--tago-device-token", dest="tago_device_token", default=TAGO_DEVICE_TOKEN,
+                        help="Tago device token.")
 
     (options, sys.argv) = parser.parse_args()
+
+    if not options.tago_device_token:
+        print >>sys.stderr, 'Invalid device token'
+        sys.exit(1)
 
     # Create a UDS socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -80,7 +85,9 @@ if __name__ == "__main__":
                     tago_header = {"Content-type": "application/json","Device-Token":options.tago_device_token}
 
                     try:
-                        requests.post(options.tago_url, json=payload, headers=tago_header, timeout=2)
+                        r = requests.post(options.tago_url, json=payload, headers=tago_header, timeout=2)
+                        print '* status code: %s' % r.status_code
+                        r.raise_for_status()
                     except:
                         # An error has occurred, likely due to a lost internet connection,
                         # and the post has failed.
